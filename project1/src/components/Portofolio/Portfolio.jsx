ddecb316f53c7ad2e0eaebe18695f6131d3b0853
allaahossam <allaahossam309@gmail.com>
2025-12-16 22:09:47 +0200

    portfolio edit

--- project1/src/components/Portofolio/Portfolio.jsx
+++ project1/src/components/Portofolio/Portfolio.jsx
@@ -1,47 +1,63 @@
-import { Modal } from 'bootstrap';
-import React, { useState } from 'react'
-
-
-
+import React from "react";
+import "./portfolio.css";
 
 export default function Portfolio() {
-
-
-  const [images, setimages] = useState([{
-     img_cover:
+  const images = [
+    {
+      img_cover:
         "https://routeegy.github.io/startFramework/assets/images/poert1.png",
-      title: "Project 1"},
-      {
-         img_cover:
+      title: "Project 1",
+    },
+    {
+      img_cover:
         "https://routeegy.github.io/startFramework/assets/images/port2.png",
-      title: "Project 2"},
-      {
-        img_cover:
+      title: "Project 2",
+    },
+    {
+      img_cover:
         "https://routeegy.github.io/startFramework/assets/images/port3.png",
-      title: "Project 3"},
-      {
-        img_cover:
+      title: "Project 3",
+    },
+    {
+      img_cover:
         "https://routeegy.github.io/startFramework/assets/images/poert1.png",
-      title: "Project 4"},
-      {
-         img_cover:
+      title: "Project 4",
+    },
+    {
+      img_cover:
         "https://routeegy.github.io/startFramework/assets/images/port2.png",
-      title: "Project 5"},
-      {
-        img_cover:
+      title: "Project 5",
+    },
+    {
+      img_cover:
         "https://routeegy.github.io/startFramework/assets/images/port3.png",
-      title: "Project 6"
-      }
-    ]);
+      title: "Project 6",
+    },
+  ];
+
   return (
     <div className="container py-5">
       <h2 className="text-center mb-4 fw-bold">PORTFOLIO COMPONENT</h2>
+      <div className="d-flex justify-content-center align-items-center mb-4">
+            <div className="lineContact mx-3"></div>
+               <i className="fa-solid fa-star fs-4"></i>
+            <div className="lineContact mx-3"></div>
+        </div>  
+       
 
       <div className="row g-4">
-        {imegs.map((item, index) => (
+        {images.map((item, index) => (
           <div className="col-md-4" key={index}>
-            <div className={styles.card}>
-              <img src={item.img_cover} alt={item.title} />
+            <div className="portfolio-card position-relative">
+              <img
+                src={item.img_cover}
+                alt={item.title}
+                className="img-fluid rounded"
+              />
+
+              <div className="overlay d-flex justify-content-center align-items-center">
+                <h4 className="text-white fw-bold">{item.title}</h4>
+              </div>
             </div>
           </div>
         ))}
